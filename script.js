14ffc3ae4ee6538c2c30bdffd912725fb05278cf
Ansh Gupta <ng607301@gmail.com>
2026-03-16 17:39:53 +0530

    pin

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-gsap.from("#page1 .box",{
-    scale:0,
-    delay:1,
-    duration:1,
-    rotate:360
-
-})
-
-gsap.from("#page2 .box",{
-    scale:0,
-    
-    duration:1,
-    rotate:360,
+gsap.to('#page2 h1',{
+    transform:"translate(-120%)",
     scrollTrigger:{
-        trigger:"#page2 .box",
+        trigger:"#page2",
         scroller:"body",
         markers:true,
-        start:"top 80%",
-        end:"bottom top"
-    }
+        start:"top 0%",
+        end:"top -100%",
+        scrub:2,
 
+        pin:true
+    }
 })
